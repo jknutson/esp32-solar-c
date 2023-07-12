9e3d756cc77b80a2b37ca0052df5f34042e4f4be
John Knutson <john.m.knutson@gmail.com>
2023-07-11 22:34:03 -0500

    docs: sql updates

--- doc/queries.sql
+++ doc/queries.sql
@@ -13,9 +13,37 @@ FROM journal
 WHERE RIGHT(topic, 11) = 'temperature'
 AND text::float < 120 AND text::float > -120-- weed out outliers/erroneous readings
 -- AND DATE(time::timestamp) = DATE(current_date) -- today
-AND DATE(time::timestamp) = (current_date - INTERVAL '1 day')::date -- yesterday
+AND DATE(time::timestamp AT TIME ZONE 'cdt') = DATE(current_date - INTERVAL '1 day') -- yesterday
 GROUP BY 1;
 
+
+CREATE UNIQUE INDEX idx_summaries_time_station on wm_daily_summaries ("date", station);
+
+CREATE OR REPLACE FUNCTION wm_update_yesterday_summaries()
+       RETURNS VOID AS 
+       $$
+      INSERT INTO wm_daily_summaries
+      SELECT
+      DATE(time::timestamptz AT TIME ZONE 'cdt') as "date",
+      MIN(text::float) as min_temperature,
+      MAX(text::float) as max_temperature,
+      AVG(text::float) as avg_temperature,
+      SPLIT_PART(topic, '/', 2) as station
+      FROM journal
+      WHERE RIGHT(topic, 11) = 'temperature'
+      AND text::float < 120 AND text::float > -120-- weed out outliers/erroneous readings
+      -- AND DATE(time::timestamp) = DATE(current_date) -- today
+      AND DATE(time::timestamptz AT TIME ZONE 'cdt') = DATE(current_date AT TIME ZONE 'cdt' - INTERVAL '1 day') -- yesterday
+      GROUP BY 1,5
+      ON CONFLICT("date", station) -- TODO: ensure this will work when we have multiple stations
+      DO UPDATE
+        SET min_temperature = EXCLUDED.min_temperature, max_temperature = EXCLUDED.max_temperature, avg_temperature = EXCLUDED.avg_temperature;
+      $$
+      LANGUAGE sql STRICT;
+
+
+
+-- select time,time::timestamp,time::timestamptz at time zone 'cst' from journal order by 1 desc limit 5;
 
 SELECT * FROM journal LIMIT 1;
 
